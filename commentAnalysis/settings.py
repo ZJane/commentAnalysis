b07915188ef528f632103172770dd5e34ce635c1
张敏华 <1578711560@qq.com>
2018-05-05 17:54:34 +0800

    settings.py change

--- commentAnalysis/settings.py
+++ commentAnalysis/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = '/home/dormitory_manage_system/venv/bin/commentAnalysis/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
