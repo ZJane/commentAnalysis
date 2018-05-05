d500c2ffab619fa8febc9805c80b5c1956a50aac
张敏华 <1578711560@qq.com>
2018-05-05 18:14:07 +0800

    settings.py change

--- commentAnalysis/settings.py
+++ commentAnalysis/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '%fk@@l%x#aazvbhx_yw=okaw0e-r@joav@rmc@__-_jjd4w#$r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
